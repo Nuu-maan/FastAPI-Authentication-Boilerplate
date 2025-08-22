745c970d6c6f774c6295dd00f4ece4ad3e150914
nuu-maan <n82238895@gmail.com>
2025-08-22 21:13:18 +0530

    Add app/schemas/user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    is_email_verified: bool
+
+    class Config:
+        from_attributes = True
