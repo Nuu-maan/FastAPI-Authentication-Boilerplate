c6c41fe1399ffa924999295f84dd9fef3043e32d
nuu-maan <n82238895@gmail.com>
2025-08-22 21:13:15 +0530

    Add app/models/role.py

---
+++ app/models/role.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.base import Base
+
+
+class Role(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, index=True)
+
+    users = relationship("User", back_populates="role")
