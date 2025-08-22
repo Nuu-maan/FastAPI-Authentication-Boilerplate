54879a8f7c792e05569528092045f4772681344c
nuu-maan <n82238895@gmail.com>
2025-08-22 21:13:21 +0530

    Add alembic/versions/23cf6eb5b6b7_init_schema.py

---
+++ alembic/versions/23cf6eb5b6b7_init_schema.py
@@ -0,0 +1,21 @@
+"""init schema
+
+Revision ID: 23cf6eb5b6b7
+Revises: 
+Create Date: 2025-08-22 20:40:13.736991
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '23cf6eb5b6b7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade() -> None:
+    pass
+
+def downgrade() -> None:
+    pass
